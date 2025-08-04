ca54a9b3a7eb8720a03354ceb1209283e357e28e
alimaazamat <alima.azamat2003@gmail.com>
2025-08-04 14:55:50 -0700

    migrated DRA driver to v1

--- cmd/dra-example-webhook/main_test.go
+++ cmd/dra-example-webhook/main_test.go
@@ -28,7 +28,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	admissionv1 "k8s.io/api/admission/v1"
-	resourceapi "k8s.io/api/resource/v1beta1"
+	resourceapi "k8s.io/api/resource/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
--- demo/scripts/common.sh
+++ demo/scripts/common.sh
@@ -41,7 +41,7 @@ SCRIPTS_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 # At present, kind has a new enough node image that we don't need to build our
 # own. This won't always be true and we may need to set the variable below to
 # 'true' from time to time as things change.
-: ${BUILD_KIND_IMAGE:="false"}
+: ${BUILD_KIND_IMAGE:="true"}
 
 # The name of the kind cluster to create
 : ${KIND_CLUSTER_NAME:="${DRIVER_NAME}-cluster"}
